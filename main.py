43a560e9530f8f6ad30adbed780ad96246f35810
Martin Vitoušek <martin.vitousek@seznam.cz>
2024-12-11 14:31:42 +0100

    Add basic implementation

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 print("Hello world")
 
 print("Hi")
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+# function to add two numbers
+def add(a, b):
+    return a + b
+
+
+# function to subtract two numbers
+def subtract(a, b):
+    return a - b
+
+
+if __name__ == "__main__":
+    print(factorial(5))
+    print(add(2, 3))
+    print(subtract(5, 2))
